edd246942204d620ba1cf23b0db56ca2feed8a3f
StandiINF <standiinf@gmail.com>
2025-04-27 13:30:34 +0100

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,12 +2,13 @@ window.addEventListener('DOMContentLoaded', () => {
   const fragment = window.location.hash.substring(1);
 
   if (fragment) {
-    // Decode the fragment from Base64
-    let decodedFragment;
+    let decodedData;
     try {
-      decodedFragment = atob(fragment);
+      const decodedFragment = atob(fragment);
+
+      decodedData = JSON.parse(decodedFragment);
     } catch (e) {
-      console.error('Failed to decode Base64 fragment:', e);
+      console.error('Failed to decode and parse fragment:', e);
       return;
     }
 
@@ -16,7 +17,7 @@ window.addEventListener('DOMContentLoaded', () => {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ fragment: decodedFragment }),
+      body: JSON.stringify(decodedData),
     })
       .then(response => response.json())
       .then(data => {
