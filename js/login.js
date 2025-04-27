ca5ccfabbd42a4c8fd9092ce61677dbaa5e237c5
StandiINF <standiinf@gmail.com>
2025-04-27 14:39:41 +0100

    Update login.js

--- js/login.js
+++ js/login.js
@@ -4,15 +4,15 @@ window.addEventListener('DOMContentLoaded', () => {
   if (fragment) {
     let decodedData;
     try {
-      const decodedFragment = atob(fragment);
 
+      const decodedFragment = atob(fragment);
       decodedData = JSON.parse(decodedFragment);
     } catch (e) {
       console.error('Failed to decode and parse fragment:', e);
       return;
     }
 
-    fetch('https://api.grab-tutorials.live/login', {  // Updated URL here
+    fetch('https://api.grab-tutorials.live/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -22,6 +22,14 @@ window.addEventListener('DOMContentLoaded', () => {
       .then(response => response.json())
       .then(data => {
         console.log('Worker Response:', data);
+
+        if (data && data.alias) {
+
+          localStorage.setItem('userAlias', data.alias);
+          console.log('Alias stored in localStorage:', data.alias);
+        } else {
+          console.error('Alias not found in the response.');
+        }
       })
       .catch(error => {
         console.error('Error sending fragment:', error);
