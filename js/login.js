521a9f5bff66321d24738e1a29e23cd543cd6730
StandiINF <standiinf@gmail.com>
2025-04-27 10:46:40 +0100

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+  const fragment = window.location.hash.substring(1);
+
+  if (fragment) {
+    fetch('https://grab-tutorials.live/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ fragment: fragment }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Worker Response:', data);
+      })
+      .catch(error => {
+        console.error('Error sending fragment:', error);
+      });
+  }
+});
